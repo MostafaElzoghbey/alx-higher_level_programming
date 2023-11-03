c9dff8e6c7a8bc18eb6ee6d9305187fbb898193a
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-11-03 16:07:54 +0200

    done

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -8,7 +8,7 @@ import math
 class MagicClass:
     """Represent a circle."""
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """Initialize a MagicClass.
 
         Arg:
