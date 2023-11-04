9e447fbd2c57394aeb4ab75eca11c53a90319c1c
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-11-04 16:44:43 +0200

    done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """class for abstract Rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width
-        self.__height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
