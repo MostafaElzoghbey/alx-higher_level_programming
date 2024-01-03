ebf666631201cb7885f32a041ef1240e3b0dc160
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2024-01-03 22:41:08 +0200

    done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,4 @@
 -- * id_not_null description:
 -- * id INT with the default value 1 and must be unique
 -- * name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS id_not_null (id INT UNIQUE default 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT unique default 1, name VARCHAR(256));
