0c014a161700b7862d9b46494abfd49d8c1b61d3
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2024-04-25 01:00:40 +0200

    done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const { argv } = require('process');
 const occurence = Number(argv[2]);
 if (isNaN(occurence)) {
-  	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < occurence; i++) {
     console.log('C is fun');
